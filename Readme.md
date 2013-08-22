2d80f51b1180fcb5e651ab31302b15c12f349a9a
Gaurav Sharma <gaurav.qwe@gmail.com>
2013-08-22 09:52:22 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,58 @@
+# XDMessage
+
+PostMesssage wrapper for IFrames and Popup windows with callback support. Tries to replicate similar functionality in easyXDM but in a smaller package.
+
+## Getting Started
+
+### Parent window code:
+
+```HTML
+
+<script src="//static.getchute.com/js/xdmessage.js"></script>
+
+<script type="text/javascript">
+  window.onload = function() {
+    var xd = new XDMessage('http://yourdomain.com/iframe.html', { container: document.getElementById('iframe-container') });
+
+    xd.on('ready', function() {
+      // invoke a method with custom data
+      xd.invoke('test-message', { "a" : "b" }, function(data) {
+        console.log(data);
+        // > { "b" : "c" }
+      });
+    });
+
+    xd.open();
+  }
+</script>
+
+<!--IFrame will be injected here -->
+<div id="iframe-container"></div>
+
+```
+
+### IFrame code:
+
+```HTML
+
+<script src="//static.getchute.com/js/xdmessage.js"></script>
+
+<script type="text/javascript">
+  window.onload = function() {
+    var xd = new XDMessage();
+
+    xd.on('ready', function() {
+      // ready
+    });
+
+    xd.on('test-message', function(data, callback){
+      console.log(data);
+      // > { "a" : "b" }
+      callback({ "b" : "c" });
+    });
+
+    xd.open();
+  }
+</script>
+
+```
---
+++ xdmessage.js
@@ -0,0 +1,267 @@
+;
+(function(){
+  /*********************************
+   Helper Messages
+  *********************************/
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+
+    if (results === null) {
+      return null;
+    } else {
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+  }
+
+  function toParam(paramsObject) {
+    var params=[];
+    for(var key in paramsObject) {
+      if (paramsObject[key]) {
+        params.push(key + "=" + encodeURIComponent(paramsObject[key]))
+      }
+    }
+    return params.join('&');
+  }
+
+  var ie = (function(){
+    var undef,
+        v = 3,
+        div = document.createElement('div'),
+        all = div.getElementsByTagName('i');
+
+    while (
+        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+        all[0]
+    );
+
+    return v > 4 ? v : undef;
+  }());
+
+  /*********************************
+   XDPost
+  *********************************/
+  var XDMessage = function(url, options) {
+    if (typeof url === 'object') {
+      options = url;
+      url = null;
+    }
+
+    this.options       = options || {};
+    this.events        = {};
+    this.callbacks     = {};
+
+    this.windowHostURL = getParameterByName('opener');
+    this.token         = getParameterByName('XDMessage_token');
+    this.isChildWindow = !url;
+
+    if (this.options.popup && ie < 10) {
+      this.options.popup = false;
+      this.log('Popups not supported in IE < 10');
+    }
+
+    if (!this.isChildWindow) {
+      this.frameURL     = url;
+      this.frameHostURL = this.frameURL.match(/\S+\/\/([^\/]+)\//)[0].slice(0,-1);
+    } else if (!this.windowHostURL) {
+      throw 'opener parameter required';
+    }
+  };
+
+  XDMessage.prototype.open = function() {
+    var self = this;
+    this._startListening();
+
+    if (this.frameURL) {
+      var opener = document.location.protocol + "//" + document.location.host;
+      this.token = ("" + Math.random()).replace('0.','');
+      var url    = this.frameURL + (this.frameURL.indexOf('?') === -1 ? '?' : '&') + toParam({ opener: opener, XDMessage_token: this.token });
+
+      if (this.options.popup) {
+        var width          = 600;
+        var height         = 500;
+        var left           = parseInt((screen.availWidth/2) - (width/2));
+        var top            = parseInt((screen.availHeight/2) - (height/2));
+        var windowFeatures = "width=" + width + ",height=" + height + ",status,resizable,scrollbars=1,left=" + left + ",top=" + top + "screenX=" + left + ",screenY=" + top;
+
+        this.popup = window.open(url, "XDMessage_popup", windowFeatures);
+      } else {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('frameborder', '0');
+        iframe.src = url;
+        iframe.onload = this._frameReady;
+
+        if (this.options.target) {
+          this.options.target.appendChild(iframe);
+        } else {
+          document.body.appendChild(iframe);
+        }
+
+        this.iframe = iframe;
+      }
+    } else {
+      this.invoke('_ready', function() {
+        self._ready();
+      });
+      this.on('_close', function(){
+        if (self.options.popup) {
+          window.close();
+        }
+      });
+    }
+    this.on('_ready', function(data, callback) {
+      callback();
+      self._ready();
+    });
+  };
+
+  XDMessage.prototype.close = function() {
+    if (this.iframe) {
+      this.iframe.parentNode.removeChild(this.iframe);
+    } else {
+      this.popup.close();
+    }
+    delete(this.events);
+    delete(this.callbacks);
+    this._stopListening();
+  };
+
+  XDMessage.prototype.on = function(event, callback) {
+    if (typeof event === 'string' && typeof callback === 'function') {
+      this.events[event] = callback;
+    }
+  };
+
+  XDMessage.prototype.send = function(data, callback, meta) {
+    if (typeof callback === 'object') {
+      meta = callback;
+      callback = null;
+    }
+
+    var __xd_post_meta = meta || {};
+    __xd_post_meta.token = this.token;
+
+    if (callback) {
+      var random = "" + Math.random();
+      this.callbacks[random] = callback;
+      __xd_post_meta.callback = random;
+    }
+
+    var message = { __xd_post_meta: __xd_post_meta, body: data };
+
+    this.log("Sending to" + (self.isChildWindow ? " parent " : " frame "));
+    this.log(message);
+
+    this._sendMessage(message);
+  };
+
+  XDMessage.prototype.invoke = function(method, data, callback, meta) {
+    if (typeof data === 'function') {
+      callback = data;
+      data = undefined;
+    }
+    this.send(data, callback, { method: method });
+  };
+
+  /*********************************
+   Private Methods
+  *********************************/
+  XDMessage.prototype._receiveMessage = function(event) {
+    var allowedURL = this.isChildWindow ? this.windowHostURL : this.frameHostURL;
+
+    if (event.origin === allowedURL) {
+      var message;
+
+      try {
+        message = JSON.parse(event.data)
+      } catch (ex) {
+        this.log('message data parsing failed, ignoring');
+      }
+
+      if (message && typeof message.__xd_post_meta !== 'undefined' && this.token === message.__xd_post_meta.token) {
+        if (typeof message.__xd_post_meta.callback_response === 'string') {
+          var callback = this.callbacks[message.__xd_post_meta.callback_response];
+          callback(message.body);
+          delete(callback);
+        } else {
+          var method;
+          if (message.__xd_post_meta.method) {
+            if (this.events[message.__xd_post_meta.method]) {
+              method = this.events[message.__xd_post_meta.method]
+            }
+          } else if (this.events.data) {
+            method = this.events.data;
+          }
+
+          if (method) {
+            if (typeof message.__xd_post_meta.callback === 'string') {
+              var self = this;
+              method(message.body, function(data){
+                self.send(data, { callback_response: message.__xd_post_meta.callback });
+              });
+            } else {
+              method(message.body);
+            }
+          }
+        }
+      }
+    }
+  };
+
+  XDMessage.prototype._sendMessage = function(data) {
+    if (this.isChildWindow) {
+      if (window.opener) {
+        window.opener.postMessage(JSON.stringify(data), this.windowHostURL);
+      } else if (window.parent) {
+        window.parent.postMessage(JSON.stringify(data), this.windowHostURL);
+      }
+    } else {
+      if (this.options.popup) {
+        this.popup.postMessage(JSON.stringify(data), this.frameHostURL);
+      } else {
+        this.iframe.contentWindow.postMessage(JSON.stringify(data), this.frameHostURL);
+      }
+    }
+  };
+
+  XDMessage.prototype._startListening = function() {
+    var self = this;
+    self.listener = function(event) {
+      self._receiveMessage(event);
+    }
+
+    if (document.addEventListener){
+      window.addEventListener('message',  self.listener, false);
+    } else {
+      window.attachEvent('onmessage', self.listener);
+    }
+  };
+
+  XDMessage.prototype._stopListening = function() {
+    if (document.removeEventListener){
+      window.removeEventListener('message', this.listener, false);
+    } else {
+      window.detachEvent('onmessage', this.listener);
+    }
+  };
+
+  XDMessage.prototype._frameReady = function() {
+  };
+
+  XDMessage.prototype._ready = function() {
+    if (this.events.ready) {
+      this.events.ready();
+      // delete(this.events.ready);
+    }
+  };
+
+  XDMessage.prototype.log = function(message) {
+    if (this.options.verbose && typeof console !== 'undefined' && typeof console.log === 'function') {
+      console.log(message);
+    }
+  };
+
+  this.XDMessage = XDMessage;
+}).call(this);
